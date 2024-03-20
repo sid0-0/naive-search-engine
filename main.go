b92dc9423fd5e050da0fa32fdf1af1f5a35af904
Siddharth <deshpandesiddharth@gmail.com>
2024-03-21 02:50:59 +0530

    sse fix

--- main.go
+++ main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"bufio"
-	"errors"
 	"fmt"
 	"log"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/template/html/v2"
+	"github.com/google/uuid"
 	"github.com/valyala/fasthttp"
 )
 
@@ -17,38 +17,37 @@ import (
 var SeedPages = []string{"https://www.wikipedia.org/"}
 
 type Subscriber struct {
-	stream *bufio.Writer
+	Id            string
+	LogChannel    chan (string)
+	ResultChannel chan (string)
+}
+
+func NewSubscriber() *Subscriber {
+	return &Subscriber{
+		Id:            uuid.NewString(),
+		LogChannel:    make(chan string),
+		ResultChannel: make(chan string),
+	}
 }
 
 var subscribersList []*Subscriber
 
-func startSending(s *Subscriber) error {
-	fmt.Println(s)
-	w := s.stream
-	if w == nil {
-		return errors.New("write stream missing")
-	}
+func startSending(s *Subscriber, w *bufio.Writer) {
 	for {
 		msg := fmt.Sprintf("Scraping %s", "wikipedia.org")
-		// fmt.Fprintf(w, "data: <h2>%s</h2>\n\n", msg)
-		fmt.Fprintf(w, "event:addLog\ndata:%s<br>\n\n", msg)
+		fmt.Fprintf(w, "event:addLog\ndata:%s<br>%s<br>\n\n", msg, s.Id)
 		fmt.Fprintf(w, "event:searchResult\ndata:<div><a href='%s' class='w-full'>%s</a></div>\n\n", "www.wikipedia.com", "wikipedia.com")
 
 		err := w.Flush()
-		if w == nil {
-			fmt.Println("write stream missing")
-		}
 		if err != nil {
 			// Refreshing page in web browser will establish a new
 			// SSE connection, but only (the last) one is alive, so
 			// dead connections must be closed here.
 			fmt.Printf("Error while flushing: %v. Closing http connection.\n", err)
-
 			break
 		}
 		time.Sleep(time.Second)
 	}
-	return nil
 }
 
 func main() {
@@ -57,7 +56,6 @@ func main() {
 		Views: engine,
 	})
 
-	// allTemplates, _ = ParseAllTemplates("templates")
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.Render("base", nil)
 	})
@@ -68,15 +66,15 @@ func main() {
 		c.Set("Connection", "keep-alive")
 		c.Set("Transfer-Encoding", "chunked")
 
-		var sub Subscriber
 		c.Context().SetBodyStreamWriter(fasthttp.StreamWriter(func(w *bufio.Writer) {
-			sub = Subscriber{stream: w}
-			subscribersList = append(subscribersList, &sub)
+			sub := NewSubscriber()
+			subscribersList = append(subscribersList, sub)
+			startSending(sub, w)
 		}))
-		_ = startSending(&sub)
 
 		return nil
 	})
+
 	// app.Get("/search", func(c *fiber.Ctx) error {
 	// 	term := c.Query("term")
 
